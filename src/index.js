017fc2a635b369e2301f7e72e7fd3cae4408cee1
Alexander Ruzhickii <ruzhickiisasha@gmail.com>
2018-11-22 22:29:33 +0200

    Preparing structure of project && installing react-router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './index.scss';
+import Layout from './modules/containers/layout/index';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//import {Router, Route, browserHistory} from 'react-router'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+
+
+ReactDOM.render(
+   <Layout />,
+    document.getElementById('root'));
+
 serviceWorker.unregister();
